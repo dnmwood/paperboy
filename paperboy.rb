b6f6f6b8052b8051898ac4d2a7c6df62d5700a6e
David Wood <davidnmwood@gmail.com>
2016-10-21 15:11:49 -0400

    delivery method created

--- paperboy.rb
+++ paperboy.rb
@@ -1,15 +1,36 @@
 class PaperBoy
 
-attr_reader
+  attr_reader
 
-def initialize(name, experience, earnings)
-  @name       = name
-  @experience = experience
-  @earnings   = earnings
+  def initialize(name, experience, earnings)
+    @name       = name
+    @experience = experience
+    @earnings   = earnings
+  end
+
+  def quota
+   @experience / 2 + 50
+  end
+
+  def deliver(start_address, end_address)
+    #calculate number of houses
+    number_of_houses = end_address - start_address
+    #calculate money made
+    money_made = number_of_houses * 0.25
+    #increase experience by number of Houses
+    @experience += number_of_houses
+    #increase total earnings by money made
+    @earnings += money_made
+  end
+
+  def report
   end
 end
 
-########################################
-timmy = PaperBoy.new("Timmy", 50, 1)
-lucas = PaperBoy.new("Lucas", 50, 1)
-ben = PaperBoy.new("Ben", 50, 1)
+timmy = PaperBoy.new("Timmy", 80, 1)
+lucas = PaperBoy.new("Lucas", 70, 1)
+ben = PaperBoy.new("Ben", 60, 1)
+
+puts timmy.quota
+puts lucas.quota
+puts ben.quota
